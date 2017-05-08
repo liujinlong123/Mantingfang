80654ff96bcb0384bcc7d0206ad4fd929c87092e
liujinlong123 <1048338958@qq.com>
2017-05-09 07:27:37 +0800

    3

--- src/com/android/mantingfang/third/FragmentThird.java
+++ src/com/android/mantingfang/third/FragmentThird.java
@@ -1,25 +1,62 @@
 package com.android.mantingfang.third;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.android.mantingfanggsc.R;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class FragmentThird extends Fragment {
 
 	private View view;
+	private ThirdShiYouListViewAdapter mAdapter;
+	private ListView listview;
+	private List<ThirdContent> list;
 	
 	@SuppressLint("InflateParams")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		if (view == null) {
 			view = inflater.inflate(R.layout.frag_third_pager, null);
+			
+			initListView();
 			return view;
 		}
 		
 		return view;
+	}
+	
+	//初始化listView
+	private void initListView() {
+		listview = (ListView)view.findViewById(R.id.third_listview);
+		mAdapter = new ThirdShiYouListViewAdapter(getActivity(), getData());
+		listview.setAdapter(mAdapter);
+		
+		listview.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Toast.makeText(getActivity(), "点击了  " + position + " 项", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+	
+	private List<ThirdContent> getData() {
+		list = new ArrayList<ThirdContent>();
+		for (int i = 0; i < 7; i++) {
+			ThirdContent content = new ThirdContent();
+			list.add(content);
+		}
+		return list;
 	}
 }
--- src/com/android/mantingfang/third/ThirdShiYouListViewAdapter.java
+++ src/com/android/mantingfang/third/ThirdShiYouListViewAdapter.java
@@ -3,14 +3,18 @@ package com.android.mantingfang.third;
 import java.util.List;
 
 import com.android.mantingfanggsc.R;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ThirdShiYouListViewAdapter extends BaseAdapter {
 
@@ -45,12 +49,34 @@ public class ThirdShiYouListViewAdapter extends BaseAdapter {
 		ViewHolder holder;
 		View view;
 		if (convertView == null) {
-			view = inflater.inflate(R.layout.activity_main, null);
+			view = inflater.inflate(R.layout.item_listview_shiyou, null);
+			holder = new ViewHolder();
+			holder.linearHead = (LinearLayout)view.findViewById(R.id.thirdlist_linear_headuser);
+			holder.headPhoto = (ImageView)view.findViewById(R.id.thirdlist_img_headPhoto);
+			holder.userName = (TextView)view.findViewById(R.id.thirdlist_tv_username);
+			holder.more = (ImageView)view.findViewById(R.id.thirdlist_img_more);
+			holder.content = (TextView)view.findViewById(R.id.thirdlist_tv_content);
+			holder.linearPoem = (LinearLayout)view.findViewById(R.id.thirdlist_linear_poem);
+			holder.poemName = (TextView)view.findViewById(R.id.thirdlist_tv_poemName);
+			holder.poemQuote = (TextView)view.findViewById(R.id.thirdlist_tv_poem);
+			holder.time = (TextView)view.findViewById(R.id.thirdlist_tv_time);
+			holder.share = (ImageView)view.findViewById(R.id.thirdlist_img_share);
+			holder.comment = (ImageView)view.findViewById(R.id.thirdlist_img_comment);
+			holder.collect = (ImageView)view.findViewById(R.id.thirdlist_img_collect);
+			
+			view.setTag(holder);
+		} else {
+			view = convertView;
+			holder = (ViewHolder)view.getTag();
 		}
-		return null;
+		
+		initViews(holder, list);
+		return view;
 	}
 	
 	final static class ViewHolder {
+		LinearLayout linearHead;
+		
 		ImageView headPhoto;
 		
 		TextView userName;
@@ -58,6 +84,10 @@ public class ThirdShiYouListViewAdapter extends BaseAdapter {
 		ImageView more;
 		
 		TextView content;
+		
+		LinearLayout linearPoem;
+		
+		TextView poemName;
 		
 		TextView poemQuote;
 		
@@ -68,8 +98,63 @@ public class ThirdShiYouListViewAdapter extends BaseAdapter {
 		ImageView comment;
 		
 		ImageView collect;
-		
-		
 	}
 
+	private void initViews(ViewHolder holder, List<ThirdContent> list) {
+		holder.headPhoto.setImageResource(R.drawable.ic_launcher);
+		holder.userName.setText("user");
+		holder.linearHead.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "跳转到用户界面", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		holder.more.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "more", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		holder.poemName.setText("静夜思");
+		holder.content.setText("窗前明月光,疑是地上霜,举杯邀明月,低头思故乡");
+		holder.poemQuote.setText("窗前明月光,\n\n疑是地上霜.\n\n举杯邀明月,\n\n低头思故乡.");
+		holder.linearPoem.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "跳转到诗", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		holder.time.setText("09:00");
+		
+		holder.share.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "share", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		holder.comment.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "comment", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		holder.collect.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(mContext, "collect", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+	}
 }
